6b5f5344dc74ed24270907ed2384a12d884d2cdb
odessaistaken <mahmutakbudak34@gmail.com>
2026-06-30 04:08:27 +0300

    fix: update base URL and router basename to SultanSarayiwebsiter1

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/sultansarayiwebsite/">
+    <BrowserRouter basename="/SultanSarayiwebsiter1/">
       <App />
     </BrowserRouter>
   </StrictMode>
